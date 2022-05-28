4af5110725975ca5161d7d0d04721cead6cd23cd
Johannes Deutsch <johannes.deutsch@gmx.at>
2022-05-28 17:31:40 +0200

    finished data.json

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('I love Treehouse!');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
     
 });
 
-app.listen(3000);
+app.get('/about', (req, res) => {
+    res.render('about');
+    
+});
+
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!');
+
+});
