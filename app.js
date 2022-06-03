d3449bddc4e1ee21986577a5f6c91e190c7d83d2
Johannes Deutsch <johannes.deutsch@gmx.at>
2022-06-03 15:10:37 +0200

    loop, projects folder
    deleted projects folder, template literals in index.pug loop

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const { data } = require('data.json');
 
 const app = express();
 
 app.set('view engine', 'pug');
 
+
+app.use((req, res, next) => {
+    console.log('One');
+    next();
+});
+
+
 app.get('/', (req, res) => {
     res.render('index');
-    
+    res.locals = data.projects;
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
     
 });
+
+
+router.get('/project', (req, res) => {
+    res.render('project', {
+        project: projects[req.params.id]
+    })
+});
+
+
 
 
 app.listen(3000, () => {
