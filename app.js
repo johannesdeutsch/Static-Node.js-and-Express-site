f429f6cc11b576068b8bb66aace0d835a3f4f8f9
Johannes Deutsch <johannes.deutsch@gmx.at>
2022-06-28 13:47:45 +0200

    try setting up express again

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const { data } = require('data.json');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.set('view engine', 'pug');
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use((req, res, next) => {
-    console.log('One');
-    next();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.get('/', (req, res) => {
-    res.render('index');
-    res.locals = data.projects;
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
-    
-});
-
-
-router.get('/project', (req, res) => {
-    res.render('project', {
-        project: projects[req.params.id]
-    })
-});
-
-
-
-
-app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!');
-
-});
+module.exports = app;
