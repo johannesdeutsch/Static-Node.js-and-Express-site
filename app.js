9be979deb9105ea6c479feda0edd1d034caa54e3
Johannes Deutsch <johannes.deutsch@gmx.at>
2022-08-15 14:43:15 +0200

    trying to solve index route

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const { projects } = require('./data/data.json');
-const bodyParser = require('body-parser');
+const { data } = require('./data/data.json');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/static', express.static('public'));
 // tells express to use pug
 app.set('view engine', 'pug');
@@ -12,7 +10,8 @@ app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.render('index');
-    res.locals = data.projects;
+    const dataProjects = data.projects;
+    res.locals.dataProjects = req.dataProjects;
 });
 
 app.get('/about', (req, res) => {
@@ -21,28 +20,38 @@ app.get('/about', (req, res) => {
 });
 
 
-app.post('/projects/:id', (req, res, next) => {
-    const project = projects[req.params.id];
+app.post('/data/:id', (req, res, next) => {
+    const project = data[req.params.id];
     if (project) {
         res.render('project', {
         project});
     } 
 }); 
 
+
+
+
+
+
 app.use((req, res, next) => {
     const err = new Error('Not Found');
-    err.status = 404;
-    err.message = 'Sorry! The page does not exist unfortunately.'
+    res.status = 404;
+    res.message = 'Sorry! The page does not exist unfortunately.'
     console.log(err.message);
     console.log(err.status);
     next(err);
 });
 
-app.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error', err);
 
+// code snippet from https://stackoverflow.com/questions/49210127/getting-rangeerror-err-http-invalid-status-code-invalid-status-code-undefine
+
+app.use((err, req, res, next) => {
+    if(err.status !== 404) {
+        err.message = err.message || 'Oops!! I looks lie something went wrong';
+        res.status(err.status || 500);
+    }
+    console.log(err.message);
+    console.log(err.status);
 });
 
 app.listen(3000, () => {
